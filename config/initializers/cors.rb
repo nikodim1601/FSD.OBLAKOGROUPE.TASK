c530014d6717e67408d5539a7ff31613c1fdcab4
nikDiq <na.dikarev@northis.ru>
2022-06-29 15:12:41 +0300

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200', 'https://nik-proj-web.herokuapp.com/'
+    origins 'http://localhost:4200', 'https://nik-proj-web.herokuapp.com'
 
     resource '*',
       headers: :any,
